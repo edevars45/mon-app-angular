ac0d594c04ccd0695f45c23bdbc51e55381547a6
Esther <devarsesther@gmail.com>
2026-02-12 15:29:54 +0100

    feat: implémentation du composant Home (liste des logements)

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filtrer par ville">
+        <button class="primary" type="button">Rechercher</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation">
+      </app-housing-location>
+    </section>
+  `,
+  styles: [`
+    .results {
+      display: grid;
+      column-gap: 20px;
+      row-gap: 20px;
+      grid-template-columns: repeat(auto-fill, minmax(400px, 400px));
+      justify-content: center;
+      margin-top: 50px;
+    }
+  `]
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
