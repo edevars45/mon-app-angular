269ebd30373c0b5a8e138c768a4efca57fa02dce
Esther <devarsesther@gmail.com>
2026-02-16 17:18:35 +0100

    feat(L11): injection du service dans le composant Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,66 +7,69 @@ import { HousingService } from '../housing';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    HousingLocationComponent
-  ],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section class="search-section">
       <form (submit)="filterResults(filter.value); $event.preventDefault()">
-        <input type="text" placeholder="Filtrer par ville" #filter>
-        <button class="primary" type="button" (click)="filterResults(filter.value)">Rechercher</button>
+        <input type="text" placeholder="Filtrer par ville" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">
+          Rechercher
+        </button>
       </form>
     </section>
-
     <section class="results">
       <app-housing-location
         *ngFor="let housingLocation of filteredLocationList"
-        [housingLocation]="housingLocation">
+        [housingLocation]="housingLocation"
+      >
       </app-housing-location>
     </section>
   `,
-  styles: [`
-    .search-section {
-      display: flex;
-      justify-content: center;
-      margin: 20px 0;
-    }
-    form {
-      display: flex;
-      width: 100%;
-      justify-content: center;
-    }
-    input[type="text"] {
-      border: solid 1px #bebebe;
-      padding: 10px;
-      border-radius: 8px;
-      margin-right: 8px;
-      width: 400px; /* Largeur fixe pour plus de clarté */
-      outline: none;
-    }
-    input[type="text"]:focus {
-      border-color: var(--primary-color);
-    }
-    .results {
-      display: grid;
-      column-gap: 20px;
-      row-gap: 20px;
-      grid-template-columns: repeat(auto-fill, minmax(400px, 400px));
-      justify-content: center;
-      margin-top: 30px;
-    }
-  `]
+  styles: [
+    `
+      .search-section {
+        display: flex;
+        justify-content: center;
+        margin: 20px 0;
+      }
+      form {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+      }
+      input[type='text'] {
+        border: solid 1px #bebebe;
+        padding: 10px;
+        border-radius: 8px;
+        margin-right: 8px;
+        width: 400px; /* Largeur fixe pour plus de clarté */
+        outline: none;
+      }
+      input[type='text']:focus {
+        border-color: var(--primary-color);
+      }
+      .results {
+        display: grid;
+        column-gap: 20px;
+        row-gap: 20px;
+        grid-template-columns: repeat(auto-fill, minmax(400px, 400px));
+        justify-content: center;
+        margin-top: 30px;
+      }
+    `,
+  ],
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   filteredLocationList: HousingLocation[] = [];
 
-  constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
-  }
+ constructor() {
+  this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+    this.housingLocationList = housingLocationList;
+    this.filteredLocationList = housingLocationList;
+  });
+}
 
   filterResults(text: string) {
     if (!text) {
@@ -74,8 +77,8 @@ export class HomeComponent {
       return;
     }
 
-    this.filteredLocationList = this.housingLocationList.filter(
-      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
     );
   }
 }
