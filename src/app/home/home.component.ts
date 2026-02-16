be096a45b9e896aa46ef6bab5806d5d424ca712c
Esther <devarsesther@gmail.com>
2026-02-16 14:00:21 +0100

    feat: ajout fonction filterResults dans HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,35 +12,70 @@ import { HousingService } from '../housing';
     HousingLocationComponent
   ],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filtrer par ville">
-        <button class="primary" type="button">Rechercher</button>
+    <section class="search-section">
+      <form (submit)="filterResults(filter.value); $event.preventDefault()">
+        <input type="text" placeholder="Filtrer par ville" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Rechercher</button>
       </form>
     </section>
+
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation">
       </app-housing-location>
     </section>
   `,
   styles: [`
+    .search-section {
+      display: flex;
+      justify-content: center;
+      margin: 20px 0;
+    }
+    form {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+    }
+    input[type="text"] {
+      border: solid 1px #bebebe;
+      padding: 10px;
+      border-radius: 8px;
+      margin-right: 8px;
+      width: 400px; /* Largeur fixe pour plus de clarté */
+      outline: none;
+    }
+    input[type="text"]:focus {
+      border-color: var(--primary-color);
+    }
     .results {
       display: grid;
       column-gap: 20px;
       row-gap: 20px;
       grid-template-columns: repeat(auto-fill, minmax(400px, 400px));
       justify-content: center;
-      margin-top: 50px;
+      margin-top: 30px;
     }
   `]
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
