b7fa284e0b3176de43f41aae2354c42006a1e958
Esther <devarsesther@gmail.com>
2026-02-18 15:51:24 +0100

    feat(L14): migration vers API fetch asynchrone

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,46 +3,40 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing';
 import { HousingLocation } from '../housinglocation';
-// Étape 2 : Import des classes pour les formulaires réactifs
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  // Étape 2 : Ajout de ReactiveFormsModule dans les imports du composant
-  imports: [
-    CommonModule,
-    ReactiveFormsModule
-  ],
-  // Étape 3 : Mise à jour du template avec le balisage du formulaire
- template: `
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo"
-        alt="Photo extérieure de {{housingLocation?.name}}"/>
+        alt="Exterior photo of {{housingLocation?.name}}"/>
       <section class="listing-description">
         <h2 class="listing-heading">{{housingLocation?.name}}</h2>
         <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
       </section>
       <section class="listing-features">
-        <h2 class="section-heading">À propos de ce logement</h2>
+        <h2 class="section-heading">About this housing location</h2>
         <ul>
-          <li>Unités disponibles : {{housingLocation?.availableUnits}}</li>
-          <li>Wi-fi disponible : {{housingLocation?.wifi ? 'Oui' : 'Non'}}</li>
-          <li>Laverie disponible : {{housingLocation?.laundry ? 'Oui' : 'Non'}}</li>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
       <section class="listing-apply">
-        <h2 class="section-heading">Postulez maintenant pour y vivre</h2>
+        <h2 class="section-heading">Apply now to live here</h2>
         <form [formGroup]="applyForm" (submit)="submitApplication()">
-          <label for="first-name">Prénom</label>
+          <label for="first-name">First Name</label>
           <input id="first-name" type="text" formControlName="firstName">
 
-          <label for="last-name">Nom</label>
+          <label for="last-name">Last Name</label>
           <input id="last-name" type="text" formControlName="lastName">
 
-          <label for="email">E-mail</label>
+          <label for="email">Email</label>
           <input id="email" type="email" formControlName="email">
-          <button type="submit" class="primary">Postuler</button>
+          <button type="submit" class="primary">Apply now</button>
         </form>
       </section>
     </article>
@@ -54,19 +48,19 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
-  // Étape 2 : Création de l'objet de formulaire (FormGroup)
   applyForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl('')
   });
 
-  constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
-  }
+ constructor() {
+  const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+  this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+    this.housingLocation = housingLocation;
+  });
+}
 
-  // Étape 2 : Méthode pour gérer le clic sur "Apply now"
   submitApplication() {
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',
